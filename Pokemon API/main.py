321ea55f79a2b9f6f1be6ce956d9afd314034591
Dilan Rojas Carmona <146038814+DilanRojasca@users.noreply.github.com>
2024-06-13 13:24:12 -0500

    Add files via upload

---
+++ Pokemon API/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class Pokemon(BaseModel):
+    id:int
+    name:str
+    type:str
+
+pokemonList = [
+    Pokemon(id=1, name="Bulbasaur", type="Grass"),
+    Pokemon(id=2, name="Ivysaur", type="Grass"),
+    Pokemon(id=3, name="Venusaur", type="Grass"),
+    Pokemon(id=4, name="Charmander", type="Fire"),
+    Pokemon(id=5, name="Charmeleon", type="Fire"),
+    Pokemon(id=6, name="Charizard", type="Fire"),
+    Pokemon(id=7, name="Squirtle", type="Water"),
+    Pokemon(id=8, name="Wartortle", type="Water"),
+    Pokemon(id=9, name="Blastoise", type="Water"),
+    Pokemon(id=10, name="Caterpie", type="Bug")
+]
+
+#endpoint #1
+@app.get("/pokemon/", response_model=Pokemon)
+async def get_pokemon():
+    return pokemonList
+
+#Endpoint #2
+@app.get("/pokemon/{id}")
+async def get_pokemon_id(id:int):
+    return search_pokemon(id)
+
+
+
+def search_pokemon(id: int):
+        get_pokemon = filter(lambda get_pokemon_id: get_pokemon_id.id == id, pokemonList)
+        try:   
+            return list(pokemonList)[0]
+        except:
+             raise HTTPException(status_code=404, detail="pokemon not found")
